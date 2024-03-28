eb9edf4a07b60e4e1b29f2cfb6a5b2a849fc3dfa
vrajarathin <vijay_rajarathinam@intuit.com>
2024-03-28 21:02:07 +0530

    fix: database config

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -25,6 +25,5 @@ class Command(BaseCommand):
                 db_up = True
             except(Psycopg2Error, OperationalError):
                 self.stdout.write("Database unavailable, waiting 1 second...")
-                time.sleep(1)    
-
+                time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -24,8 +24,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """ test waiting for database when getting OperationalError. """
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 + [True]    
-        
+            [OperationalError] * 3 + [True]
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
